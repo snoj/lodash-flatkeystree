4521a50090dd2ba4100d6a6fb2bb0a5cde50b32e
Josh Erickson <josh@snoj.us>
2017-10-31 21:50:58 -0500

    first attempt at browser support.
    Signed-off-by: Josh Erickson <josh@snoj.us>

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
-var _ = require('lodash');
+(function(factory) {
+  if(typeof process != 'undefined' && typeof process.pid != 'undefined' && typeof require == 'function') {
+    var ld = require('lodash');
+    module.exports = {keysDeep: factory(ld)};
+  } else if(typeof window != 'undefined' && typeof _ != 'lodash') {
+    _.mixin({keysDeep: factory(_)});
+  }
+})(function(_) {
+  return function keysDeep(obj, depth) {
+    depth || (depth = 1);
+    var r = {};
+    var objTraversed = [];
+    function keysDeepInternal(o, treepath) {
+      if(o.__keysDeepTraversed == 0) {
+        return;
+      }
 
-function keysDeep(obj, depth) {
-  depth || (depth = 1);
-  var r = {};
-  var objTraversed = [];
-  function keysDeepInternal(o, treepath) {
-    if(o.__keysDeepTraversed == 0) {
-      return;
-    }
 
-
-    if(o.__keysDeepTraversed == -1)
+      if(o.__keysDeepTraversed == -1)
       o.__keysDeepTraversed = depth;
 
-    if(typeof o.__keysDeepTraversed == 'undefined')
+      if(typeof o.__keysDeepTraversed == 'undefined')
       Object.defineProperty(o, '__keysDeepTraversed', {enumerable:false, value: depth, writable: true});
 
-    o.__keysDeepTraversed--;
+      o.__keysDeepTraversed--;
 
-    objTraversed.push(o);
+      objTraversed.push(o);
 
-    treepath || (treepath = "");
+      treepath || (treepath = "");
 
-    _.each(o, function(v, k) {
-      var nk = (treepath.length > 0 ? treepath + "." : "") + k;
-      switch(true) {
-        case typeof v == 'string':
-        case typeof v == 'number':
-        case typeof v == 'function':
-        case typeof v == 'boolean':
-        case typeof v == 'undefined':
-        case v instanceof RegExp:
-        case v instanceof Promise:
-        case v === null:
+      _.each(o, function(v, k) {
+        var nk = (treepath.length > 0 ? treepath + "." : "") + k;
+        switch(true) {
+          case typeof v == 'string':
+          case typeof v == 'number':
+          case typeof v == 'function':
+          case typeof v == 'boolean':
+          case typeof v == 'undefined':
+          case v instanceof RegExp:
+          case v instanceof Promise:
+          case v === null:
           r[nk] = v;
           return;
-      }
-      keysDeepInternal(v, nk);
+        }
+        keysDeepInternal(v, nk);
+      });
+    }
+    keysDeepInternal(obj, "");
+
+    //cleanup traversal flag
+    _.each(objTraversed, function(o) {
+      o.__keysDeepTraversed = -1;
     });
+
+    return _.keys(r);
   }
-  keysDeepInternal(obj, "");
 
-  //cleanup traversal flag
-  _.each(objTraversed, function(o) {
-    o.__keysDeepTraversed = -1;
-  });
-
-  return _.keys(r);
-}
-
-module.exports = {keysDeep: keysDeep};
+})
